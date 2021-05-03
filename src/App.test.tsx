9d9723ba1d4ec67287b9dfb565fdbe41bd7922e5
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2021-05-03 19:59:59 -0300

    fix: correct testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getAllByText(/Conheça a Jussi/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
