3fbf4b953c2176e4c897c87363223470e34f1722
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2021-05-03 19:34:45 -0300

    feat: Sticky menu implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Sticky } from 'semantic-ui-react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import GlobalStyle from './styles/global';
+import AppProvider from './hooks';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppProvider>
+        <Sticky context={document.getElementById('root')}>
+          <Header />
+        </Sticky>
+        <Home />
+      </AppProvider>
+
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
